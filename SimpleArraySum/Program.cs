066c60123327daaebdb4520b533af794e0d1c422
Orest Syniavskyi <orestsyn@gmail.com>
2018-09-12 12:57:12 +0300

    first commit

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int sum = 0;
+            for(int i = 0;i<array.Length;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                sum += array[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
